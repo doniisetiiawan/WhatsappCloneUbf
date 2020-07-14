83c3d4054f3869cbffbcbc75a3ecb403fc827ecd
Doni Setiawan <donisetiawan.050@gmail.com>
2020-07-14 16:36:37 +0700

    Laying out our app

--- src/screens/ChatScreen.jsx
+++ src/screens/ChatScreen.jsx
@@ -6,14 +6,30 @@ import {
   StyleSheet,
   Button,
   FlatList,
+  ImageBackground,
 } from 'react-native';
 import { getMockData } from '../services/api';
+import background from '../assets/imgs/background.png';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: 'transparent',
+    width: '100%',
+  },
+  listItem: {
+    width: '70%',
+    margin: 10,
+    padding: 10,
+    backgroundColor: 'white',
+    borderColor: '#979797',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 10,
+  },
+  incomingMessage: {
+    alignSelf: 'flex-end',
+    backgroundColor: '#E1FFC7',
   },
 });
 
@@ -38,26 +54,35 @@ export default class Home extends React.Component {
     });
   };
 
+  getMessageRow = (item) => (
+    <View
+      style={[
+        styles.listItem,
+        item.incoming
+          ? styles.incomingMessage
+          : styles.outgoingMessage,
+      ]}
+    >
+      <Text>{item.message}</Text>
+    </View>
+  );
+
   render() {
     return (
-      <View style={styles.container}>
-        <View>
-          <Text>Chat with John</Text>
-        </View>
+      <ImageBackground
+        style={styles.container}
+        source={background}
+      >
         <FlatList
           data={this.state.messages}
-          renderItem={({ item }) => (
-            <View>
-              <Text>{item.message}</Text>
-            </View>
-          )}
+          renderItem={({ item }) => this.getMessageRow(item)}
           keyExtractor={(item, index) => `message-${index}`}
         />
         <Button
           onPress={() => this.props.navigation.goBack()}
           title="Navigate to Home Screen"
         />
-      </View>
+      </ImageBackground>
     );
   }
 }
